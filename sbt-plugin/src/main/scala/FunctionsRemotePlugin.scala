6aa8c2adc0f8dcf7b6fa71084907c77f4205961f
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-02 15:14:18 +0000

    -

--- sbt-plugin/src/main/scala/FunctionsRemotePlugin.scala
+++ sbt-plugin/src/main/scala/FunctionsRemotePlugin.scala
@@ -7,17 +7,21 @@ object FunctionsRemotePlugin extends AutoPlugin {
   override def trigger  = noTrigger // Not automatically enabled
 
   object autoImport {
-//    val helloGreeting = settingKey[String]("greeting")
-    val functionsRemoteDependencies = taskKey[Unit]("say hello")
+    val functionsRemoteReceiver               = settingKey[Boolean]("Set to true to generate Receiver classes")
+    val functionsRemoteCreateDependenciesFile = taskKey[Unit]("say hello")
   }
 
   import autoImport.*
 
-  override lazy val globalSettings: Seq[Setting[_]]  = Seq(
-//    helloGreeting := "hi"
-  )
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
-    functionsRemoteDependencies := {
+    functionsRemoteReceiver               := false,
+    Compile / unmanagedSourceDirectories ++= {
+      val base = baseDirectory.value
+      if (functionsRemoteReceiver.value)
+        Seq(base / "src" / "main" / "functions-remote-generated")
+      else Seq.empty
+    },
+    functionsRemoteCreateDependenciesFile := {
       val s        = streams.value
       val org      = organization.value
       val sv       = scalaBinaryVersion.value
@@ -29,13 +33,13 @@ object FunctionsRemotePlugin extends AutoPlugin {
       functions.coursier.Resolver.createDependenciesForArtifact(artifact)
     },
     // hook into publishLocal so that after publishing we create functions-remote dependency file
-    publishLocal                := Def.taskDyn {
+    publishLocal                          := Def.taskDyn {
       val s  = streams.value
       val pl = publishLocal.value
       Def.task {
         s.log.info(pl.toString)
         // Run your task actions here after publishLocal has completed
-        functionsRemoteDependencies.value
+        functionsRemoteCreateDependenciesFile.value
         pl
       }
     }.value
